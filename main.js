bf6f9b3df053b0403097f650ecbc278d33f45c5c
Abdulloh <abdullaevabdulohav@gmail.com>
2025-05-27 15:02:24 +0500

    done with calcularor have issue when type 0 and .

---
+++ main.js
@@ -0,0 +1,68 @@
+const number = document.querySelectorAll('td');
+const show = document.querySelector('.space')
+const arr = {
+    first: undefined,
+    sign: undefined,
+    second: undefined,
+};
+function isNumber(value) {
+  return !isNaN(value);
+}
+number.forEach( number => {
+    number.addEventListener("click", (number) => {
+    const textNumber = number.target.textContent;
+    if (show.textContent === '0'){
+        show.textContent = '';
+    }
+    if (isNumber(textNumber) && isNumber(show.textContent)){
+        show.textContent += textNumber;
+        if (show.textContent === '0'){
+            show.textContent = '';
+        }
+    }
+    else{
+        if(arr.first === undefined){
+            arr.first = Number(show.textContent);
+        }
+        else if(arr.sign && !arr.second){
+            arr.second = Number(show.textContent);
+        }
+        show.textContent = textNumber;
+        if('+-×%÷'.includes(textNumber)){
+            arr.sign = textNumber;            
+        }
+        else if(textNumber === 'AC'){
+            for(const key in arr){
+                arr[key] = undefined;
+            }
+            show.textContent = '0';
+        }
+        else if(textNumber === '='){
+            let temp = 0;
+            if (arr.sign === '+' ){
+                temp = arr.first + arr.second;
+                show.textContent = temp;
+            }
+            else if (arr.sign === '-' ){
+                temp = arr.first - arr.second;
+                show.textContent = temp;
+            }
+            else if (arr.sign === '×' ){
+                temp = arr.first * arr.second;
+                show.textContent = temp;
+            }
+            else if (arr.sign === '÷' ){
+                temp = arr.first / arr.second;
+                show.textContent = temp;
+            }
+            else if (arr.sign === '%' ){
+                temp = arr.first / 100 * arr.second;
+                show.textContent = temp;
+            }
+            arr.first = show.textContent;
+            arr.sign = undefined;
+            arr.second = undefined;
+        }
+    }
+})
+});
